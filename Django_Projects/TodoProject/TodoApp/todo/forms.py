0a118a4dc4bf8c7bf09c728413b6467db68a257a
Faheem Aslam <faheem17171@gmail.com>
2025-01-02 14:22:34 -0600

    Deleted the todoProject

--- Django_Projects/TodoProject/TodoApp/todo/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class TodoForm(forms.Form):
-    text = forms.CharField(max_length=100,widget=forms.TextInput(
-            attrs={'class' : 'form-control', 'placeholder' : 'Add', 'aria-label' : 'Todo', 'aria-describedby' : 'add-btn'}))
